c6ef66ca73159bc2245645b6d7ff211c0a7773ee
derakoptes <75810359+Derakoptes@users.noreply.github.com>
2025-09-26 15:38:06 +0100

    Version Change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.acube.audii"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.2"
+        versionCode = 11
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
